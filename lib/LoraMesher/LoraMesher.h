e026a4eeb32219a4291b2bb933b62cb35d46dd1c
guillemllongarriu <guille54x@gmail.com>
2022-06-20 13:36:27 +0200

    fix git

---
+++ lib/LoraMesher/LoraMesher.h
@@ -0,0 +1,541 @@
+#ifndef __LORAMESHER__
+#define __LORAMESHER__
+
+// LoRa libraries
+#include "RadioLib.h"
+
+// WiFi libraries
+#include <WiFi.h>
+
+// Logger
+//#define DISABLE_LOGGING
+#include <ArduinoLog.h>
+
+
+// LoRa transceiver module pins
+#define SCK 5
+#define MISO 19
+#define MOSI 27
+#define SS 18
+#define RST 14
+#define DIO0 26
+
+// LoRa band definition
+// 433E6 for Asia
+// 866E6 for Europe
+// 915E6 for North America
+#define BAND 868.0
+#define LORASF 7 // Spreading factor 6-12 (default 7)
+
+// Comment this line if you want to remove the reliable payload
+#define RELIABLE_PAYLOAD
+
+// SSD1306 OLED display pins
+// For TTGO T3_V1.6: SDA/SCL/RST 21/22/16
+// For Heltec ESP32: SDA/SCL/RST 4/15/16
+
+#ifdef ARDUINO_TTGO_LoRa32_V1
+#define HASOLEDSSD1306 true
+#define HASGPS false
+#define GPS_SDA 0
+#define GPS_SCL 0
+//#define OLED_SDA 21
+//#define OLED_SCL 22
+#define OLED_RST 16
+#endif
+
+#ifdef ARDUINO_HELTEC_WIFI_LORA_32
+#define HASOLEDSSD1306 true
+#define HASGPS false
+#define GPS_SDA 0
+#define GPS_SCL 0
+#define OLED_SDA 4
+#define OLED_SCL 15
+#define OLED_RST 16
+#endif
+
+#ifdef ARDUINO_TTGO_T1
+#define HASOLEDSSD1306 true
+#define HASGPS true
+#define GPS_SDA 21
+#define GPS_SCL 22
+#define OLED_RST 0
+#define OLED_SDA 21
+#define OLED_SCL 22
+#endif
+
+// Routing table max size
+#define RTMAXSIZE 256
+#define MAXPAYLOADSIZE 200 //In bytes
+
+// Packet types
+#define HELLO_P 0x04
+#define DATA_P 0x03
+#define PAYLOADONLY_P 0x02
+
+// Packet configuration
+#define BROADCAST_ADDR 0xFFFF
+#define DEFAULT_PRIORITY 20
+
+//Time out by default 60s
+#define DEFAULT_TIMEOUT 60
+
+class LoraMesher {
+
+public:
+  /**
+   * @brief Construct a new Lora Mesher object
+   *
+   * @param receiverFunction Receiver function. It will be notified when data for the user is habailable.
+   * Example of usage:
+    void processReceivedPackets(void* parameters) {
+     for (;;) {
+        //Wait for the notification of processReceivedPackets and enter blocking
+        ulTaskNotifyTake(pdPASS, portMAX_DELAY);
+
+        // Get the receivedUserPackets and get all the elements
+        while (radio->ReceivedUserPackets->Size() > 0) {
+          LoraMesher::packetQueue<dataPacket>* packetReceived = radio->ReceivedUserPackets->Pop<dataPacket>();
+
+          //Do something with the packet, ex: print(packetReceived);
+
+        }
+      }
+    }
+
+    Then initialize:
+    LoraMesher radio = new LoraMesher(processReceivedPackets);
+   */
+  LoraMesher(void (*receiverFunction)(void*));
+
+  /**
+   * @brief Destroy the Lora Mesher
+   *
+   */
+  ~LoraMesher();
+
+  /**
+   * @brief Prints the actual routing table in the log
+   *
+   */
+  void printRoutingTable();
+
+  /**
+   * @brief Returns the routing table size
+   *
+   * @return int
+   */
+  int routingTableSize();
+
+  /**
+   * @brief Returns if address is inside the routing table
+   *
+   * @param address Addres you want to check if is inside the routing table
+   * @return true If the address is inside the routing table
+   * @return false If the addres is not inside the routing table
+   */
+  bool hasAddresRoutingTable(uint16_t address);
+
+  /**
+   * @brief Get the Next Hop address
+   *
+   * @param dst address of the next hop
+   * @return uint16_t address of the next hop
+   */
+  uint16_t getNextHop(uint16_t dst);
+
+  /**
+   * @brief Get the Local Address
+   *
+   * @return uint16_t Address
+   */
+  uint16_t getLocalAddress();
+
+
+
+#pragma pack(push, 1)
+  template <typename T>
+  struct packet {
+    uint16_t dst;
+    uint16_t src;
+    uint8_t type;
+    uint8_t payloadSize = 0;
+    T payload[];
+  };
+
+  template <typename T>
+  struct dataPacket {
+    uint16_t via;
+    T payload[];
+  };
+
+#pragma pack(pop)
+
+  /**
+   * @brief Create a Packet And Send it
+   *
+   * @tparam T
+   * @param dst Destination
+   * @param src Source, normally local addres, use getLocalAddress()
+   * @param type Type of packet
+   * @param payload Payload of type T
+   * @param payloadSize Length of the payload in T
+   */
+  template <typename T>
+  void createPacketAndSend(uint16_t dst, T* payload, uint8_t payloadSize) {
+    uint8_t type = dst == BROADCAST_ADDR ? PAYLOADONLY_P : DATA_P;
+    setPackedForSend(createPacket(dst, getLocalAddress(), type, payload, payloadSize), DEFAULT_PRIORITY, DEFAULT_TIMEOUT);
+  }
+
+  /**
+   * @brief Get the Payload of the packet
+   *
+   * @tparam T type of the payload
+   * @param packet Packet that you want to get the payload
+   * @return T* pointer of the packet payload
+   */
+  template <typename T>
+  T* getPayload(packet<T>* packet) {
+    if (packet->type == DATA_P)
+      return (T*) (((dataPacket<T>*) (&packet->payload))->payload);
+
+    return (T*) packet->payload;
+  }
+
+  /**
+   * @brief Create a Packet<T>
+   *
+   * @tparam T
+   * @param dst Destination
+   * @param src Source, normally local addres, use getLocalAddress()
+   * @param type Type of packet
+   * @param payload Payload of type T
+   * @param payloadSize Length of the payload in T
+   * @return struct LoraMesher::packet<T>*
+   */
+  template <typename T>
+  packet<T>* createPacket(uint16_t dst, uint16_t src, uint8_t type, T* payload, uint8_t payloadSize) {
+    bool hasVia = type == DATA_P;
+    packet<T>* p = createPacket(payload, payloadSize, hasVia);
+    p->dst = dst;
+    p->type = type;
+    p->src = src;
+
+    return p;
+  };
+
+  /**
+   * @brief Create a Packet<T>
+   *
+   * @tparam T
+   * @param payload Payload of type T
+   * @param payloadSize Length of the payload in T
+   * @param hasVia Indicates the function that it need to add the via
+   * @return struct LoraMesher::packet<T>*
+   */
+  template <typename T>
+  packet<T>* createPacket(T* payload, uint8_t payloadSize, bool hasVia) {
+    int payloadLength = payloadSize * sizeof(T);
+
+    int viaSize = (hasVia ? sizeof(dataPacket<uint32_t>::via) : 0);
+
+    //Packet length = size of the packet + size of the payload + if has via -> size of via
+    int packetLength = sizeof(packet<T>) + payloadLength + viaSize;
+
+    packet<T>* p = (packet<T>*) malloc(packetLength);
+
+    if (p) {
+      //Copy the payload into the packet
+      //If hasVia, we need to add the sizeof via to point correctly to the p->payload
+      memcpy((void*) ((unsigned long) p->payload + (viaSize)), payload, payloadLength);
+
+      p->payloadSize = payloadLength;
+    }
+
+    Log.trace(F("Packet created with %d bytes" CR), packetLength);
+
+    return p;
+  };
+
+  /**
+   * @brief Delete the packet from memory
+   *
+   * @tparam T Type of packet
+   * @param p Packet to delete
+   */
+  template <typename T>
+  void deletePacket(packet<T>* p) {
+    free(p);
+  }
+
+  /**
+   * @brief Sets the packet in a Fifo with priority and will send the packet when needed.
+   *
+   * @tparam T
+   * @param p packet of type T
+   * @param priority Priority set DEFAULT_PRIORITY by default. 0 most priority
+   * @param timeout Packet timeout, DEFAULT_TIMEOUT
+   */
+  template <typename T>
+  void setPackedForSend(packet<T>* tx, uint8_t priority, uint32_t timeout) {
+    packetQueue<uint32_t>* send = createPacketQueue<uint32_t>(tx, priority, timeout);
+    ToSendPackets->Add(send);
+    //TODO: Using vTaskDelay to kill the packet inside LoraMesher
+  }
+
+  /**
+   * @brief Get the payload in number of elements
+   *
+   * @tparam T
+   * @param p
+   * @return size_t
+   */
+  template<typename T>
+  size_t getPayloadLength(packet<T>* p){
+    return p->payloadSize / sizeof(T);
+  }
+
+  /**
+   * @brief packetQueue template
+   *
+   * @tparam T
+   */
+#pragma pack(push, 1)
+  template <typename T>
+  struct packetQueue {
+    uint32_t timeout;
+    uint8_t priority = DEFAULT_PRIORITY;
+    LoraMesher::packet<T>* packet;
+    packetQueue<T>* next;
+  };
+#pragma pack(pop)
+
+  /**
+   * @brief Create a Packet Queue element
+   *
+   * @tparam T type of the packet queue
+   * @tparam I type of the packet
+   * @param p packet
+   * @param priority priority inside the queue
+   * @param timeout timeout in s
+   * @return packetQueue<T>*
+   */
+  template <typename T, typename I>
+  packetQueue<T>* createPacketQueue(packet<I>* p, uint8_t priority, uint32_t timeout) {
+    packetQueue<T>* pq = new packetQueue<T>();
+    pq->timeout = micros() + timeout * 1000;
+    pq->priority = priority;
+    pq->packet = (packet<T>*) p;
+    pq->next = nullptr;
+    return pq;
+  }
+
+  class PacketQueue {
+    packetQueue<uint32_t>* first = nullptr;
+
+  public:
+    /**
+     * @brief Add pq in order of priority
+     *
+     * @param pq
+     */
+    void Add(packetQueue<uint32_t>* pq);
+
+    /**
+     * @brief Get the first packetQueue of the list and deletes it from the list
+     *
+     * @tparam T
+     * @return packetQueue<T>*
+     */
+    template <typename T>
+    packetQueue<T>* Pop() {
+      if (first == nullptr)
+        return nullptr;
+
+      packetQueue<T>* firstCp = (packetQueue<T>*) first;
+      first = first->next;
+      return firstCp;
+    }
+
+    /**
+     * @brief Returns the size of the queue
+     *
+     * @return size_t
+     */
+    size_t Size();
+
+    /**
+     * @brief Clear the packetQueueFifo, deleting all the packets inside too.
+     *
+     */
+    void Clear();
+  };
+
+  LoraMesher::PacketQueue* ReceivedUserPackets = new PacketQueue();
+
+private:
+
+  PacketQueue* ReceivedPackets = new PacketQueue();
+  PacketQueue* ToSendPackets = new PacketQueue();
+
+#pragma pack(push, 1)
+  struct networkNode {
+    uint16_t address = 0;
+    uint8_t metric = 0;
+  };
+
+  struct routableNode {
+    LoraMesher::networkNode networkNode;
+    unsigned long timeout = 0;
+    uint16_t via = 0;
+  };
+#pragma pack(pop)
+
+  //Routing table
+  routableNode routingTable[RTMAXSIZE];
+  //Local Address
+  uint16_t localAddress;
+  // Duty cycle end
+  unsigned long dutyCycleEnd;
+  // Time for last HELLO packet
+  unsigned long lastSendTime;
+  // Routable node timeout (µs)
+  unsigned long routeTimeout;
+
+  SX1276* radio;
+
+  TaskHandle_t Hello_TaskHandle = NULL;
+  TaskHandle_t ReceivePacket_TaskHandle = NULL;
+
+  TaskHandle_t ReceiveData_TaskHandle = NULL;
+  TaskHandle_t SendData_TaskHandle = NULL;
+
+  TaskHandle_t ReceivedUserData_TaskHandle = NULL;
+
+
+  void onReceive();
+
+  void receivingRoutine();
+
+  void initializeLocalAddress();
+
+  void initializeLoRa();
+
+  void initializeScheduler(void (*func)(void*));
+
+  void sendHelloPacket();
+
+  /**
+   * @brief Function that process the packets inside Received Packets
+   * Task executed every time that a packet arrive.
+   *
+   */
+  void processPackets();
+
+  // void dataCallback();
+
+  // void helloCallback();
+
+  /**
+   * @brief process the network node, adds the node in the routing table if can
+   *
+   * @param via via address
+   * @param node networkNode
+   */
+  void processRoute(uint16_t via, LoraMesher::networkNode* node);
+
+  /**
+   * @brief Process the network packet
+   *
+   * @param p Packet of type networkNode
+   */
+  void processRoute(LoraMesher::packet<networkNode>* p);
+
+  /**
+   * @brief Add node to the routing table
+   *
+   * @param node Network node that includes the address and the metric
+   * @param via Address to next hop to reach the network node address
+   */
+  void addNodeToRoutingTable(LoraMesher::networkNode* node, uint16_t via);
+
+  /**
+   * @brief Create a Routing Packet adding the routing table to the payload
+   *
+   * @return struct LoraMesher::packet*
+   */
+  struct LoraMesher::packet<networkNode>* createRoutingPacket();
+
+  /**
+   * @brief Send a packet through Lora
+   *
+   * @tparam T Generic type
+   * @param p Packet
+   */
+  template <typename T>
+  void sendPacket(packet<T>* p) {
+    Log.trace(F("Sending packet of type %d" CR), p->type);
+    radio->clearDio0Action(); //For some reason, while transmitting packets, the interrupt pin is set with a ghost packet
+
+    Log.trace(F("About to transmit packet" CR));
+    //TODO: Change this to startTransmit as a mitigation to the wdt error so we can raise the priority of the task. We'll have to look on how to start to listen for the radio again
+    int res = radio->transmit((uint8_t*) p, getPacketLength(p));
+
+    if (res != 0) {
+      Log.error(F("Transmit gave error: %d" CR), res);
+    }
+    else {
+      Log.trace("Packet sent" CR);
+    }
+
+    radio->setDio0Action(std::bind(&LoraMesher::onReceive, this));
+    res = radio->startReceive();
+    if (res != 0)
+      Log.error(F("Receiving on end of packet transmission gave error: %d" CR), res);
+  }
+
+  /**
+   * @brief Proccess that sends the data inside the FIFO
+   *
+   */
+  void sendPackets();
+
+  /**
+   * @brief Get the Packet Length
+   *
+   * @tparam T
+   * @param p Packet of Type T
+   * @return size_t Packet size in bytes
+   */
+  template <typename T>
+  size_t getPacketLength(packet<T>* p){
+    return sizeof(packet<T>) + p->payloadSize + (p->type == DATA_P ? sizeof(dataPacket<uint32_t>::via) : 0);
+  }
+
+  /**
+   * @brief Print the packet in the Log verbose, if the type is not defined it will print the payload in Hexadecimals
+   *
+   * @tparam T
+   * @param p Packet of Type T
+   * @param received If true it will print received, else will print Created
+   */
+  template <typename T>
+  void printPacket(packet<T>* p, bool received){
+  Log.verbose(F("-----------------------------------------\n"));
+  Log.verbose(F("Current Packet: %s\n"), received ? "Received" : "Created");
+  Log.verbose(F("Destination: %X\n"), p->dst);
+  Log.verbose(F("Source: %X\n"), p->src);
+  Log.verbose(F("Type: %d\n"), p->type);
+  Log.verbose(F("----Packet of size %d bytes----\n"), getPacketLength(p));
+  switch (p->type) {
+    case HELLO_P:
+      for (int i = 0; i < getPayloadLength((packet<networkNode>*) p); i++)
+        Log.verbose(F("%d ->(%u) Address: %X - Metric: %d" CR), i, &p->payload[i], ((networkNode*) &p->payload[i])->address, ((networkNode*) &p->payload[i])->metric);
+
+  }
+
+  Log.verbose(F("-----------------------------------------\n"));
+}
+};
+
+#endif
